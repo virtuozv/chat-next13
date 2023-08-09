083159b148d53146faf7cfec75cdcd4735d6f8f6
AD\pvorobiev <voidstein@gmail.com>
2023-08-09 18:24:35 +0300

    formatted styles

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -17,9 +17,40 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   console.log({ currentUser });
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
-      <nav className=" mt-4 flex flex-col justify-between">
-        <ul role="list" className="flex flex-col items-center space-y-1">
+    <div
+      className="
+        hidden
+        lg:fixed
+        lg:inset-y-0
+        lg:left-0
+        lg:z-40
+        lg:w-20
+        xl:px-6
+        lg:overflow-y-auto
+        lg:bg-white
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col justify-between
+    "
+    >
+      <nav
+        className="
+        mt-4
+        flex
+        flex-col
+        justify-between
+        "
+      >
+        <ul
+          role="list"
+          className="
+            flex
+            flex-col
+            items-center
+            space-y-1
+        "
+        >
           {routes.map((item) => (
             <DesktopItem
               key={item.label}
@@ -43,7 +74,10 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
       >
         <nav
           onClick={() => setIsOpen(true)}
-          className="cursor-pointer hover:opacity-75 transition"
+          className="
+            cursor-pointer
+            hover:opacity-75
+            transition"
         >
           <Avatar user={currentUser} />
         </nav>
